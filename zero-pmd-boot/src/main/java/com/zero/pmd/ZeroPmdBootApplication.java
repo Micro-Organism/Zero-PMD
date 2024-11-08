0a4266c56e9e6d4ba37715f7580e94fe5ca4e60d
Micro-Organism <coolrabbit1993@gmail.com>
2024-11-08 09:47:44 +0800

    feat: springboot 集成 pmd

---
+++ zero-pmd-boot/src/main/java/com/zero/pmd/ZeroPmdBootApplication.java
@@ -0,0 +1,13 @@
+package com.zero.pmd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZeroPmdBootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZeroPmdBootApplication.class, args);
+    }
+
+}
---
+++ zero-pmd-boot/src/test/java/com/zero/pmd/ZeroPmdBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zero.pmd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZeroPmdBootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
